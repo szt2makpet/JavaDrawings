188291fdc3fa78f6a6a8d70cf46c11dbf2f4ebc1
szt2makpet <szt2makpet@vasvari.org>
2023-10-02 12:30:01 +0200

    testAa

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,17 @@ import java.awt.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Main {
-    public static void drawImage(Graphics graphics) {
-        int size = WIDTH / 2;
-        /*
-        graphics.setColor(new Color(0, 49, 63));
-        graphics.fillRect(0, 0, WIDTH, HEIGHT);
-        graphics.setColor(Color.yellow);
-        */
-        drawSomething(0, 0, size, graphics);
-    }
-
-    public static void drawSomething(int x, int y ,int size, Graphics graphics) {
-        int step = size / 14;
-
-
-        graphics.setColor(Color.red);
-        graphics.drawLine(size, y, x, y + size);
-
-        graphics.setColor(new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256)));
-        graphics.fillRect((int) (Math.random() * (WIDTH - size)), (int) (Math.random() * (HEIGHT - size)), size, size);
-
-        graphics.setColor(Color.green);
-        graphics.drawOval(x, y, size, size);
-
-
-    }
-
-    // ezt a részt nem kell módosítani
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
+    static int WIDTH = 400;
+    static int HEIGHT = 400;
+    static int rectangleX = 4;  // x-coordinate of the lower-left corner
+    static int rectangleY = 3;  // y-coordinate of the lower-left corner
+    static int rectangleWidth = 5;  // Width of the rectangle
+    static int rectangleHeight = 4; // Height of the rectangle
 
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
+        JFrame jFrame = new JFrame("Rectangle on Cartesian Plane");
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
+        CartesianPlanePanel panel = new CartesianPlanePanel();
         panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         jFrame.add(panel);
         jFrame.setLocationRelativeTo(null);
@@ -45,11 +22,22 @@ public class Main {
         jFrame.pack();
     }
 
-    static class ImagePanel extends JPanel {
+    static class CartesianPlanePanel extends JPanel {
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
-            drawImage(graphics);
+
+            int centerX = WIDTH / 2;
+            int centerY = HEIGHT / 2;
+
+            // Draw the Cartesian plane axes
+            graphics.setColor(Color.black);
+            graphics.drawLine(0, centerY, WIDTH, centerY);
+            graphics.drawLine(centerX, 0, centerX, HEIGHT);
+
+            // Draw the rectangle
+            graphics.setColor(Color.blue);
+            graphics.drawRect(rectangleX * 40 + centerX, centerY - (rectangleY + rectangleHeight) * 40, rectangleWidth * 40, rectangleHeight * 40);
         }
     }
 }
