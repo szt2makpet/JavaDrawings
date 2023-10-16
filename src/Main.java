ca95eaf6331dd6333c6ca6574f75af7ead040214
szt2makpet <szt2makpet@vasvari.org>
2023-10-16 23:29:28 +0200

    alsó kis háromszögek meg a nagy.

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,47 @@
 import javax.swing.*;
 import java.awt.*;
 
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
 public class Main {
-    static int WIDTH = 400;
-    static int HEIGHT = 400;
-    static int rectangleX = 4;  // x-coordinate of the lower-left corner
-    static int rectangleY = 3;  // y-coordinate of the lower-left corner
-    static int rectangleWidth = 5;  // Width of the rectangle
-    static int rectangleHeight = 4; // Height of the rectangle
-
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Rectangle on Cartesian Plane");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        CartesianPlanePanel panel = new CartesianPlanePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Triangle with Small Triangles");
+            frame.setSize(300, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    static class CartesianPlanePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
+            JPanel panel = new JPanel() {
+                @Override
+                protected void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    drawTriangleWithSmallTriangles(g);
+                }
 
-            int centerX = WIDTH / 2;
-            int centerY = HEIGHT / 2;
+                private void drawTriangleWithSmallTriangles(Graphics g) {
+                    int x1 = 150; 
+                    int y1 = 50;  
+                    int size = 200;
 
-            // Draw the Cartesian plane axes
-            graphics.setColor(Color.black);
-            graphics.drawLine(0, centerY, WIDTH, centerY);
-            graphics.drawLine(centerX, 0, centerX, HEIGHT);
+                    int[] xPoints = {x1, x1 - size / 2, x1 + size / 2};
+                    int[] yPoints = {y1, y1 + size, y1 + size};
 
-            // Draw the rectangle
-            graphics.setColor(Color.blue);
-            graphics.drawRect(rectangleX * 40 + centerX, centerY - (rectangleY + rectangleHeight) * 40, rectangleWidth * 40, rectangleHeight * 40);
-        }
+                    g.setColor(Color.black);
+                    g.drawPolygon(xPoints, yPoints, 3);
+
+                    int smallTriangleSize = 11; 
+                    int numberOfSmallTriangles = size / smallTriangleSize;
+
+                    for (int i = 0; i < numberOfSmallTriangles; i++) {
+                        int xSmall = x1 - size / 2 + i * smallTriangleSize - smallTriangleSize / 2 + 1;
+                        int ySmall = y1 + size - smallTriangleSize;
+                        int[] xSmallPoints = {xSmall, xSmall - smallTriangleSize / 2, xSmall + smallTriangleSize / 2};
+                        int[] ySmallPoints = {ySmall, ySmall + smallTriangleSize, ySmall + smallTriangleSize};
+
+                        g.drawPolygon(xSmallPoints, ySmallPoints, 3);
+                    }
+                }
+            };
+
+            frame.add(panel);
+            frame.setVisible(true);
+        });
     }
 }
