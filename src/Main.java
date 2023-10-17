af5107054f9740d9cde90786eaaf426a5f8f7e46
szt2makpet <szt2makpet@vasvari.org>
2023-10-17 23:50:21 +0200

    fractalTriangles

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main {
+public class Main extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int size = 300;
+        int levels = 10; // A háromszög szintjeinek száma
+
+        drawTriangle(g, 0, 0, size, levels);
+    }
+
+    private void drawTriangle(Graphics g, int x, int y, int size, int level) {
+        if (level == 0) {
+            int[] xPoints = {x, x + size / 2, x + size};
+            int[] yPoints = {y + size, y, y + size};
+            g.drawPolygon(xPoints, yPoints, 3);
+        } else {
+            int halfSize = size / 2;
+
+            drawTriangle(g, x, y, halfSize, level - 1); // Bal alsó háromszög
+            drawTriangle(g, x + halfSize, y, halfSize, level - 1); // Jobb alsó háromszög
+            drawTriangle(g, x + halfSize / 2, y + size / 2, halfSize, level - 1); // Felső háromszög
+        }
+    }
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Triangle with Small Triangles");
-            frame.setSize(300, 300);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            JPanel panel = new JPanel() {
-                @Override
-                protected void paintComponent(Graphics g) {
-                    super.paintComponent(g);
-                    drawTriangleWithSmallTriangles(g);
-                }
-
-                private void drawTriangleWithSmallTriangles(Graphics g) {
-                    int x1 = 150; 
-                    int y1 = 50;  
-                    int size = 200;
-
-                    int[] xPoints = {x1, x1 - size / 2, x1 + size / 2};
-                    int[] yPoints = {y1, y1 + size, y1 + size};
-
-                    g.setColor(Color.black);
-                    g.drawPolygon(xPoints, yPoints, 3);
-
-                    int smallTriangleSize = 11; 
-                    int numberOfSmallTriangles = size / smallTriangleSize;
-
-                    for (int i = 0; i < numberOfSmallTriangles; i++) {
-                        int xSmall = x1 - size / 2 + i * smallTriangleSize - smallTriangleSize / 2 + 1;
-                        int ySmall = y1 + size - smallTriangleSize;
-                        int[] xSmallPoints = {xSmall, xSmall - smallTriangleSize / 2, xSmall + smallTriangleSize / 2};
-                        int[] ySmallPoints = {ySmall, ySmall + smallTriangleSize, ySmall + smallTriangleSize};
-
-                        g.drawPolygon(xSmallPoints, ySmallPoints, 3);
-                    }
-                }
-            };
-
-            frame.add(panel);
-            frame.setVisible(true);
-        });
+        JFrame frame = new JFrame("Triangle Drawing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Main());
+        frame.setSize(400, 400);
+        frame.setVisible(true);
     }
 }
